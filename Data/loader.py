3d7cf293eb97adbfb9dcf31ed73295363c221d9e
Abu Darda <abu.darda@brainstation-23.com>
2024-05-31 16:02:56 +0600

    support  for argument passing

--- Data/loader.py
+++ Data/loader.py
@@ -1,14 +1,13 @@
 from langchain_community.document_loaders import SeleniumURLLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-import streamlit as st
-from langchain_community.llms import Ollama
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
-from dotenv import load_dotenv
 from langchain_community.document_loaders import DataFrameLoader
 from langchain_community.document_loaders import PyPDFLoader
 import mysql.connector
 import pandas as pd
+import argparse
+from models import database
 
 def get_urls(file):
         f = open(file,'r').read()
@@ -33,12 +32,6 @@ def load_pdf(file):
     return split_embed_save(loader)
 
 def load_db(host, user, password, database, table):
-    # conn = mysql.connector.connect(
-    #     host='127.0.0.1',
-    #     user='root',
-    #     password='',
-    #     database='products'
-    # )
     conn = mysql.connector.connect(
         host=host,
         user=user,
@@ -52,16 +45,30 @@ def load_db(host, user, password, database, table):
     return split_embed_save(loader)
 
 
-
-# def load_db(file):
-
-def make_db(urls, pdf):
-    db = load_db('127.0.0.1', 'root', '', 'products', 'cb_products')
+def make_db(urls, pdf,db_creds):
+    # db = load_db('127.0.0.1', 'root', '', 'products', 'cb_products')
+    db = load_db(db_creds.host, db_creds.user, db_creds.password, db_creds.database, db_creds.table)
     url_db = load_urls(urls)
     pdf_db = load_pdf(pdf)
     db.merge_from(url_db)
     db.merge_from(pdf_db)
-    db.save_local("embedded_knowledge_base")
+    db.save_local("Data/embedded_knowledge_base")
+    pkl = db.serialize_to_bytes()
+    pkl.save("Data/knoiwledge_base_serials.pkl")
+
+
+
 
 if __name__ == "__main__":
-    make_db("Data/urls.txt", "Data/Warranty.pdf")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-u", "--url", help = "Urls Path", default="Data/urls.txt")
+    parser.add_argument("-p", "--pdf", help = "Pdf Path", default="Data/Warranty.pdf")
+    parser.add_argument("-ht", "--host", help = "Host", default="127.0.0.1")
+    parser.add_argument("-n", "--user", help = "User", default="root")
+    parser.add_argument("-ps", "--password", help = "Password", default="")
+    parser.add_argument("-d", "--database", help = "Database", default="products")
+    parser.add_argument("-t", "--table", help = "Table", default="cb_products")
+    args = parser.parse_args()
+    # print(args)
+    db_creds = database(args.host, args.user, args.password, args.database, args.table)
+    make_db("Data/urls.txt", "Data/Warranty.pdf",db_creds)
